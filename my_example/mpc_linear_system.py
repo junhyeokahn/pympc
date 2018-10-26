c2a6b59b6c1f6e3d73c9a17bcedb293a83968c70
junhyeokahn <junhyeokahn91@gmail.com>
2018-10-26 11:45:47 -0500

    my_mcais

---
+++ my_example/mpc_linear_system.py
@@ -0,0 +1,14 @@
+# external imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+# internal imports
+from pympc.geometry.polyhedron import Polyhedron
+from pympc.dynamics.discrete_time_systems import LinearSystem
+from pympc.control.controllers import ModelPredictiveController
+from pympc.plot import plot_input_sequence, plot_state_trajectory, plot_state_space_trajectory
+
+def main(arg1):
+
+if __name__ == "__main__":
+    main()
